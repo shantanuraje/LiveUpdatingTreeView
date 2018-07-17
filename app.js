6691e749b5ce49f8678f4c7561a6728b04a0b71b
Shantanu Rajenimbalkar <shantanu.rajenimbalkar@gmail.com>
2018-07-17 10:37:50 -0400

    two way binding working, created dialog for adding a factory

--- app.js
+++ app.js
@@ -1,63 +1,77 @@
-let treeViewApp = angular.module('TreeViewApp', []);
-
-treeViewApp.controller('TreeViewController', function ($scope) {
-    $scope.tree = {name: 'Root', 
-                    factories: [{name: 'Leah', children: []},
-                                {name: 'Suzanne', children: []},
-                                {name: 'Roberto', children: []},
-                                {name: 'Greg', children: []},
-                                {name: 'Lynn', children: []}]};
-
-    $scope.arr = getRandomArbitraryNumbers(10,100,10);
-    console.log($scope.arr);
-    $scope.addFactory = function () {
-        var dialog = document.querySelector('dialog');
-        console.log(dialog);
-        dialog.showModal();
-        
-        let newFactoryName = dialog.querySelector('#factory_name');
-        let numOfChildren = dialog.querySelector('#number_of_children');
-        let lowerBound = dialog.querySelector('#lower_bound');
-        let upperBound = dialog.querySelector('#upper_bound');
-
-        console.log(newFactoryName, numOfChildren, lowerBound, upperBound);
-
-        dialog.querySelector('.ok').addEventListener('click', function () {
-            console.log(newFactoryName.value, numOfChildren.value, lowerBound.value, upperBound.value);
-            let newFactory = {name: newFactoryName.value, children: getRandomArbitraryNumbers(lowerBound.value, upperBound.value, numOfChildren.value)};
-            $scope.tree.factories.push(newFactory);
-            console.log($scope.tree);
-            dialog.close();
-        });
+let myApp = angular.module('TreeViewApp', ['ngMaterial', 'ngMessages']);
 
 
-        dialog.querySelector('.close').addEventListener('click', function() {
-          dialog.close();
-        });
-        console.log($scope.tree);
-    }
-
-    
-});
+myApp.controller('MainController', mainController);
+myApp.controller('dialogController', dialogController);
 
 
-// $scope.tree = {name: 'Root', 
-// factories: [{factoryName: 'Leah', children: []},
-//             {factoryName: 'Suzanne', children: []},
-//             {factoryName: 'Roberto', children: []},
-//             {factoryName: 'Greg', children: []},
-//             {factoryName: 'Lynn', children: []}]};
+function mainController($scope, $mdDialog) {
+  $scope.tree = {
+    name: 'Root',
+    factories: [{ name: 'Leah', children: getRandomArbitraryNumbers(0,100,8) },
+    { name: 'Suzanne', children: getRandomArbitraryNumbers(23,67,10) },
+    { name: 'Roberto', children: getRandomArbitraryNumbers(33,90,9) },
+    { name: 'Greg', children: getRandomArbitraryNumbers(12,999,2) },
+    { name: 'Lynn', children: getRandomArbitraryNumbers(89,9999,15) }]
+  };
+  console.log($scope.tree.factories);
+  
 
-function getRandomArbitraryNumbers(min, max, numOfChildren) {
-    let result = [];
-    while(numOfChildren > 0){
-        result.push( Math.floor(Math.random() * (max - min) + min));
-        numOfChildren--;
+  $scope.showDialog = function (ev) {
+    // Appending dialog to document.body to cover sidenav in docs app
+    $mdDialog.show({
+      controller: dialogController,
+      templateUrl: 'add-factory-dialog.html',
+      parent: angular.element(document.body),
+      // targetEvent: ev,
+      clickOutsideToClose: true,
+      fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
 
-    }
-    return result;
+    })
+      .then(function (answer) {
+        let newFactoryData = { name: answer.factoryName, children: getRandomArbitraryNumbers(answer.lowerBound, answer.upperBound, answer.numOfChildren) };
+        $scope.tree.factories.push(newFactoryData);
+      }, function () {
+        $scope.newFactoryData = 'You cancelled the dialog.';
+      });
+  };
+}
+
+function dialogController($scope, $mdDialog) {
+  //$mdDialog.show()
+  $scope.newFactoryData = {
+    factoryName: '',
+    numOfChildren: '',
+    lowerBound: '',
+    upperBound: ''
   }
 
-// function addFactory(factoryName, numOfChildren, lowerBound, upperBound){
+  console.log("hey");
+  $scope.hide = function () {
+    $mdDialog.hide();
+  };
 
-// }
+  $scope.cancel = function () {
+    $mdDialog.cancel();
+  };
+
+  $scope.answer = function () {
+    $mdDialog.hide($scope.newFactoryData);
+  };
+
+}
+
+
+
+function getRandomArbitraryNumbers(min, max, numOfChildren) {
+  let result = [];
+  while(numOfChildren > 0){
+      let num = Math.floor(Math.random() * (max - min) + min)
+      if(!result.includes(num)){
+        result.push(num);
+        numOfChildren--;
+      }
+
+  }
+  return result;
+}
