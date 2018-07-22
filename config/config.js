51caf6110234c98b5d5db7b6e3ea7de93c1d82eb
Shantanu Rajenimbalkar <shantanu.rajenimbalkar@gmail.com>
2018-07-22 19:31:52 -0400

    changed production db url to use ec2

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ var config = {
     },
     "production": {
         port: 3000,
-        db: 'mongodb://127.0.0.1:27017/test',
+        db: 'mongodb://newtreeviewuser:newtreeviewpass@ec2-18-191-252-51.us-east-2.compute.amazonaws.com:27017/test',
         host: 'ec2-18-191-252-51.us-east-2.compute.amazonaws.com'
     }
 };
