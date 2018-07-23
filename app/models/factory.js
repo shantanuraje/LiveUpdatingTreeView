cc78145e782aa714be9393ab1002b62eb84f0afe
Shantanu Rajenimbalkar <shantanu.rajenimbalkar@gmail.com>
2018-07-23 19:37:48 -0400

    Refactoring factory.js, added comments to explain validator functions

--- app/models/factory.js
+++ app/models/factory.js
@@ -1,3 +1,6 @@
+//Mongoose Schema definition for factories
+//Includes custom validator functions for each factory field
+
 let mongoose = require('mongoose');
 let factorySchema = new mongoose.Schema({
     name: {
@@ -7,6 +10,7 @@ let factorySchema = new mongoose.Schema({
         required: [true, 'Factory Name is required'],
         validate: {
             validator: function (value) {
+                //check if factory name is null or contains any special characters 
                 if (value == "" || value.match(/[^a-zA-Z0-9 ]/)) {
                     return false;
                 } else {
@@ -23,6 +27,7 @@ let factorySchema = new mongoose.Schema({
         required: [true, 'Number of Children is required'],
         validate: {
             validator: function (value) {
+                //check if numOfChildren is not a number, or if its value is less than 1, or if its value is greater than 15, or if it is not a number  
                 if (typeof parseInt(value) != 'number' || value < 1 || value > 15 || isNaN(parseInt(value))) {
                     return false;
                 } else {
@@ -38,6 +43,7 @@ let factorySchema = new mongoose.Schema({
         validate: [
             {
                 validator: function (value) {
+                    //check if lowerBound is a number, and it is not undefined, and it is a number, and that it is finite
                     if (typeof parseFloat(value) === 'number' && value !== undefined && !isNaN(parseFloat(value)) && isFinite(parseFloat(value))) {
                         return true;
                     } else {
@@ -48,6 +54,7 @@ let factorySchema = new mongoose.Schema({
             },
             {
                 validator: function (value) {
+                    //check if lowerBound is lesser than the upperBound
                     return this.upperBound > value
                 },
                 message: 'Lower bound is greater than or equal to Upper bound'
@@ -61,6 +68,7 @@ let factorySchema = new mongoose.Schema({
         validate: [
             {
                 validator: function (value) {
+                    //check if upperBound is a number, and it is not undefined, and it is a number, and that it is finite
                     if (typeof parseFloat(value) === 'number' && value !== undefined && !isNaN(parseFloat(value)) && isFinite(parseFloat(value))) {
                         return true;
                     } else {
@@ -71,6 +79,7 @@ let factorySchema = new mongoose.Schema({
             },
             {
                 validator: function (value) {
+                    //check if upperBound is greater than the lowerBound
                     return this.lowerBound < value
                 },
                 message: 'Lower bound is greater than or equal to Upper bound'
