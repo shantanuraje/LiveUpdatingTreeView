249188e895f63dd1a50752cf99ada46e6cf82ca3
Shantanu Rajenimbalkar <shantanu.rajenimbalkar@gmail.com>
2018-07-23 07:36:51 -0400

    client side validation working

--- public/js/controllers/newFactoryController.js
+++ public/js/controllers/newFactoryController.js
@@ -1,56 +1,108 @@
 function newFactoryController($scope, $mdDialog) {
 
-  $scope.factoryNameInValid = false;
-  $scope.numOfChildrenInValid = false;
-  $scope.lowerBoundInValid = false;
-  $scope.upperBoundInValid = false;
+  // $scope.factoryNameInValid = false;
+  // $scope.numOfChildrenInValid = false;
+  // $scope.lowerBoundInValid = false;
+  // $scope.upperBoundInValid = false;
 
-    $scope.newFactoryData = {
-      name: '',
-      numOfChildren: '',
-      lowerBound: '',
-      upperBound: '',
-      children: []
-    }
-
-    console.log("newFactoryController");
-    $scope.hide = function () {
-      $mdDialog.hide();
-    };
-  
-    $scope.cancel = function () {
-      $mdDialog.cancel();
-    };
-
-    $scope.validateFactory = function () {
-      if ($scope.newFactoryData.name == "") {
-        $scope.factoryNameInValid = true;
-      }else{
-        $scope.factoryNameInValid = false;
-      }
-      if (typeof $scope.newFactoryData.numOfChildren != 'number'|| $scope.newFactoryData.numOfChildren < 1 || $scope.newFactoryData.numOfChildren > 15) {
-        $scope.numOfChildrenInValid = true;
-      }else{
-        $scope.numOfChildrenInValid = false;
-      }
-      if ((typeof $scope.newFactoryData.lowerBound != 'number') || ($scope.newFactoryData.lowerBound > $scope.newFactoryData.upperBound)) {
-        $scope.lowerBoundInValid = true;
-      }else{
-        $scope.lowerBoundInValid = false;
-      }
-      if((typeof $scope.newFactoryData.upperBound != 'number') || ($scope.newFactoryData.upperBound < $scope.newFactoryData.upperBound)){
-        $scope.upperBoundInValid = true;
-      }else{
-        $scope.upperBoundInValid = false;
-      }
-      return $scope.factoryNameInValid || $scope.numOfChildrenInValid || $scope.lowerBoundInValid || $scope.upperBoundInValid;
-
-    }
-  
-    $scope.answer = function () {
-      // if (!$scope.validateFactory()) {
-      // }
-      $mdDialog.hide($scope.newFactoryData);       
-    };
-  
+  $scope.newFactoryData = {
+    name: '',
+    numOfChildren: '',
+    lowerBound: '',
+    upperBound: '',
+    children: []
   }
+
+  console.log("newFactoryController");
+  $scope.hide = function () {
+    $mdDialog.hide();
+  };
+
+  $scope.cancel = function () {
+    $mdDialog.cancel();
+  };
+
+//   factoryNameInValid
+// numOfChildrenInValid
+// lowerBoundInValid
+// upperBoundInValid
+  $scope.validateFactoryName = function () {
+    if ($scope.newFactoryData.name == "" || $scope.newFactoryData.name.match(/[^a-zA-Z0-9 ]/)) {
+      $scope.factoryNameInValid = true;
+      console.log("not valid");
+      return false;
+      
+    } else {
+      $scope.factoryNameInValid = false;
+      console.log("valid");
+      return true;
+      
+    }
+  }
+
+  $scope.validateNumOfChildren = function () {
+    if (typeof parseInt($scope.newFactoryData.numOfChildren) != 'number'  || $scope.newFactoryData.numOfChildren < 1 || $scope.newFactoryData.numOfChildren > 15 || isNaN(parseInt($scope.newFactoryData.numOfChildren))) {
+      $scope.numOfChildrenInValid = true;
+      console.log("numOfChildren not valid");
+      return false;
+      
+    } else {
+      $scope.numOfChildrenInValid = false;
+      console.log("numOfChildren is a number");
+      return true;
+    }
+  }
+
+  $scope.validateLowerBound = function name() {
+    console.log($scope.newFactoryData.lowerBound, typeof $scope.newFactoryData.lowerBound);
+    if (typeof parseInt($scope.newFactoryData.lowerBound) === 'number' && $scope.newFactoryData.lowerBound !== undefined && !isNaN(parseInt($scope.newFactoryData.lowerBound)) && isFinite(parseInt($scope.newFactoryData.lowerBound))) {
+      $scope.lowerBoundInValid = false;
+      console.log("lowerBound is valid");
+      return true;
+    } else {
+      $scope.lowerBoundInValid = true;
+      console.log("lowerBound not valid");
+      return false;
+    }
+  }
+
+  $scope.validateUpperBound = function name() {
+    console.log($scope.newFactoryData.upperBound, typeof $scope.newFactoryData.upperBound);
+    if (typeof parseInt($scope.newFactoryData.upperBound) === 'number' && $scope.newFactoryData.upperBound !== undefined && !isNaN(parseInt($scope.newFactoryData.upperBound)) && isFinite(parseInt($scope.newFactoryData.upperBound))) {
+      $scope.upperBoundInValid = false;
+      console.log("upperBound is valid");
+      return true;
+    } else {
+      $scope.upperBoundInValid = true;
+      console.log("upperBound not valid");
+      return false;
+    }
+  }
+
+  $scope.validateLowerLessThanUpper = function () {
+    if (parseInt($scope.newFactoryData.lowerBound) >= parseInt($scope.newFactoryData.upperBound)) {
+      $scope.lowerNotLessThanUpper = true;
+      console.log("lower bound greater than or equal to upperbound: invalid");
+      return false;
+      
+    } else {
+      $scope.lowerNotLessThanUpper = false;
+      console.log("lower bound less than upperbound: valid");
+      return true;
+      
+    }
+  }
+
+  $scope.answer = function () {
+    if ($scope.validateFactoryName() && $scope.validateNumOfChildren() && $scope.validateLowerBound() && $scope.validateUpperBound()) {
+      if ($scope.validateLowerLessThanUpper()) {
+          $mdDialog.hide($scope.newFactoryData);
+          console.log("awesome");
+          
+      }
+    }else{
+      console.log("something went wrong");
+    }
+  };
+
+}
--- server.js
+++ server.js
@@ -11,7 +11,6 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
-var port = process.env.PORT || 3000;
 
 var mongoose = require('mongoose');
 var Factory = require('./app/models/factory.js');
